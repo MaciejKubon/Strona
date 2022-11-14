1e1de03b7949f44b3094fff1d883a9d07af2237f
Maciej <maciejk2233@gmail.com>
2022-11-14 18:53:29 +0100

    Podstawa

---
+++ script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');    
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY >=0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    window.scrollTo(0,0);
+    if(window.scrollY >=0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
